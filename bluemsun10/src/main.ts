83e657b69d1089863c044ef45b2c7c2ef06c9a8f
bigcabbag <150114793+bigcabbag@users.noreply.github.com>
2024-11-26 21:39:52 +0800

    在全局进行删除token和role

--- bluemsun10/src/main.ts
+++ bluemsun10/src/main.ts
@@ -1,5 +1,5 @@
 import './assets/main.css';
-import { createApp } from 'vue';
+import { createApp, onMounted } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
@@ -7,10 +7,12 @@ import { createPinia } from 'pinia'
 import router from './router/index';
 import App from './App.vue';
 import '../icon/iconfont/iconfont.css';
+import clearLocalStorageMixin from './mixins/clearLocalStorage';
 const app = createApp(App); 
 const pinia=createPinia();
 app.use(ElementPlus, {locale: zhCn});
 // 路由
 app.use(router);
-app.use(pinia)
+app.use(pinia) 
+app.mixin(clearLocalStorageMixin);
 app.mount('#app');
